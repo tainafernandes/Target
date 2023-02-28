10ef6511bd96ba96f323bd6892e5366e84395bd2
tainafernandes <tainafefe@gmail.com>
2023-02-28 14:46:23 -0300

    First commit

---
+++ src/test/java/io/github/tainafernandes/Target/TargetApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tainafernandes.Target;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

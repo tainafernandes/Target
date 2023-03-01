6c39798eb73e349266a11ea639094b22daed7efb
tainafernandes <tainafefe@gmail.com>
2023-03-01 13:47:26 -0300

    Finished the fifth question

---
+++ src/main/java/io/github/tainafernandes/Target/questions/FifthQuestion.java
@@ -0,0 +1,22 @@
+package io.github.tainafernandes.Target.questions;
+
+import java.util.Scanner;
+
+public class FifthQuestion {
+    public static void main(String[] args) {
+        String phrase = "Eu amo comer";
+        String phrase2 = "Três pratos de trigo para três tigres tristes.";
+
+        System.out.println(reversePhrase(phrase));
+        System.out.println(reversePhrase(phrase2));
+    }
+
+    private static String reversePhrase(String phrase) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = phrase.length() - 1; i >= 0; i--){
+            sb.append(phrase.charAt(i));
+        }
+        return sb.toString();
+    }
+}
